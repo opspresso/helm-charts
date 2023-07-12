075af1e123fe5ff9356ca7d604702adf8e57de80
nalbam <me@nalbam.com>
2023-07-12 16:01:02 +0900

    get chart name

--- bump.py
+++ bump.py
@@ -5,8 +5,8 @@ import os
 import json
 
 
-def get_charts(name):
-    txt = os.popen("helm search repo '{}' -o json".format(name)).read()
+def get_charts(chart):
+    txt = os.popen("helm search repo '{}' -o json".format(chart)).read()
 
     return json.loads(txt)
 
@@ -21,21 +21,27 @@ def main():
             doc = json.load(file)
 
             for k in doc["versions"]:
-                name = doc["versions"][k]["chart"]
+                chart = doc["versions"][k]["chart"]
 
                 old_ver = doc["versions"][k]["version"]
                 old_app = ""
                 if "app_version" in doc["versions"][k]:
                     old_app = doc["versions"][k]["app_version"]
 
+                old_txt = old_ver + " (" + old_app + ")"
+
                 new_ver = ""
                 new_app = ""
 
+                if "fixed" in doc["versions"][k] and doc["versions"][k]["fixed"] == True:
+                    print("{:45} {:20} -- {:20}".format(chart, old_txt, "fixed"))
+                    continue
+
                 # search
-                charts = get_charts(name)
+                charts = get_charts(chart)
 
                 for one in charts:
-                    if one["name"] == name:
+                    if one["name"] == chart:
                         new_ver = one["version"]
                         new_app = one["app_version"]
 
@@ -44,13 +50,12 @@ def main():
                     doc["versions"][k]["version"] = new_ver
                     doc["versions"][k]["app_version"] = new_app
 
-                    old_txt = old_ver + " (" + old_app + ")"
                     new_txt = new_ver + " (" + new_app + ")"
 
                     if new_ver != old_ver or new_app != old_app:
-                        print("{:45} {:20} -> {:20}".format(name, old_txt, new_txt))
+                        print("{:45} {:20} -> {:20}".format(k, old_txt, new_txt))
                     else:
-                        print("{:45} {:20}".format(name, old_txt))
+                        print("{:45} {:20}".format(k, old_txt))
 
         if doc != None:
             with open(filepath, "w") as file:
