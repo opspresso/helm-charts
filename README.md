c3ea383aebc71070f51af80d1f3fdce7331a3dfd
nalbam <me@nalbam.com>
2024-10-17 16:03:42 +0900

    Refactor chart version retrieval and printing logic

--- README.md
+++ README.md
@@ -40,3 +40,34 @@ aws ecr-public get-login-password \
      --username AWS \
      --password-stdin public.ecr.aws
 ```
+
+## versions
+
+<!--- BEGIN_VERSION --->
+| NAME | CURRENT | LATEST |
+| --- | --- | --- |
+| argo-cd | 6.7.2 | 7.6.10 (v2.12.4) |
+| argo-events |  | 2.4.8 (v1.9.2) |
+| argo-rollouts |  | 2.37.7 (v1.7.2) |
+| argo-workflows |  | 0.42.5 (v3.5.11) |
+| atlantis | 4.23.4 | 5.7.0 (v0.30.0) |
+| aws-ebs-csi-driver | 2.28.1 | 2.36.0 (1.36.0) |
+| aws-efs-csi-driver |  | 3.0.8 (2.0.7) |
+| aws-load-balancer-controller | 1.7.1 | 1.9.1 (v2.9.1) |
+| aws-node-termination-handler | 0.22.0 | 0.24.1 (1.22.1) |
+| awx-operator |  |  |
+| cert-manager | v1.14.4 | v1.16.1 (v1.16.1) |
+| cluster-autoscaler | 9.35.0 | 9.43.0 (1.31.0) |
+| cluster-overprovisioner |  | 0.7.11 (3.9) |
+| coroot |  | 0.15.9 (1.5.8) |
+| cost-analyzer |  | 2.4.1 (2.4.1) |
+| datadog |  | 3.74.3 (7) |
+| external-secrets | 0.9.13 | 0.10.4 (v0.10.4) |
+| ingress-nginx |  | 4.11.3 (1.11.3) |
+| istiod | 1.19.4 | 1.23.2 (1.23.2) |
+| karpenter |  | 1.0.6 (1.0.6) |
+| keda |  | 2.15.1 (2.15.1) |
+| kubernetes-dashboard | 7.1.2 | 7.8.0 |
+| metrics-server | 3.12.0 | 3.12.2 (0.7.2) |
+| raw |  | 0.2.5 (0.2.3) |
+<!--- END_VERSION --->
--- bump.py
+++ bump.py
@@ -48,7 +48,7 @@ def get_latest_version(chart, name, type="https"):
     return new_ver, new_app
 
 
-def print_version(name, old_ver, old_app, new_ver, new_app, is_fixed=False):
+def print_version(name, old_ver, old_app, new_ver, new_app):
     old_txt = old_ver
     if old_app != "":
         old_txt = old_ver + " (" + old_app + ")"
@@ -58,43 +58,92 @@ def print_version(name, old_ver, old_app, new_ver, new_app, is_fixed=False):
         new_txt = new_ver + " (" + new_app + ")"
 
     if new_ver != old_ver or new_app != old_app:
-        if is_fixed:
-            print(
-                "{:45} {:20} -- {}{:20}{}".format(name, old_txt, GRAY, new_txt, RESET)
-            )
-        else:
-            print("{:45} {:20} -> {:20}".format(name, old_txt, new_txt))
+        print("{:45} {:20} -> {}{:20}{}".format(name, old_txt, YELLOW, new_txt, RESET))
     else:
-        print("{:45} {:20}".format(name, old_txt))
+        print("{:45} {}{:20}{}".format(name, YELLOW, old_txt, RESET))
+
+    version_line = "| {} | {} | {} |".format(name, old_txt, new_txt)
+
+    return version_line
+
+
+def update_version_in_readme(version_content):
+    file_path = "README.md"
+
+    # 파일을 읽어들입니다.
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # 시작 및 종료 태그를 정의합니다.
+    begin_tag = "<!--- BEGIN_VERSION --->"
+    end_tag = "<!--- END_VERSION --->"
+
+    # 시작 및 종료 태그의 위치를 찾습니다.
+    begin_index = content.find(begin_tag)
+    end_index = content.find(end_tag)
+
+    # 태그가 존재하는지 확인합니다.
+    if begin_index == -1 or end_index == -1:
+        raise ValueError("BEGIN_VERSION 또는 END_VERSION 태그를 찾을 수 없습니다.")
+
+    # 새로운 콘텐츠를 삽입합니다.
+    new_content = (
+        content[: begin_index + len(begin_tag)]
+        + "\n"
+        + version_content
+        + "\n"
+        + content[end_index:]
+    )
+
+    # 파일에 새로운 내용을 씁니다.
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(new_content)
+
+
+def get_local_version(chart_path):
+    version = ""
+    app_version = ""
+
+    if os.path.exists(chart_path):
+        with open(chart_path, "r") as file:
+            chart_doc = yaml.unsafe_load(file)
+
+            if "version" in chart_doc:
+                version = chart_doc["version"]
+
+            if "appVersion" in chart_doc:
+                app_version = chart_doc["appVersion"]
+
+    return version, app_version
 
 
 def main():
-    filepath = "versions.json"
+    file_path = "versions.json"
 
-    if os.path.exists(filepath):
+    if os.path.exists(file_path):
         doc = None
 
-        with open(filepath, "r") as file:
+        version_contents = []
+        version_contents.append("| NAME | CURRENT | LATEST |")
+        version_contents.append("| --- | --- | --- |")
+
+        with open(file_path, "r") as file:
             doc = json.load(file)
 
             for k in doc["versions"]:
                 chart = doc["versions"][k]["chart"]
 
-                old_ver = doc["versions"][k]["version"]
-                old_app = ""
-                if "app_version" in doc["versions"][k]:
-                    old_app = doc["versions"][k]["app_version"]
+                if "path" in doc["versions"][k]:
+                    path = doc["versions"][k]["path"]
+                else:
+                    path = k
+
+                chart_path = "../argocd-env-addons/charts/" + path + "/Chart.yaml"
+
+                old_ver, old_app = get_local_version(chart_path)
 
                 new_ver = ""
                 new_app = ""
-
-                is_fixed = False
-
-                if (
-                    "fixed" in doc["versions"][k]
-                    and doc["versions"][k]["fixed"] == True
-                ):
-                    is_fixed = True
 
                 type = "https"
                 if "type" in doc["versions"][k]:
@@ -105,15 +154,18 @@ def main():
 
                 # replace
                 if new_ver != "":
-                    if is_fixed != True:
-                        doc["versions"][k]["version"] = new_ver
-                        doc["versions"][k]["app_version"] = new_app
+                    doc["versions"][k]["version"] = new_ver
+                    doc["versions"][k]["app_version"] = new_app
 
-                print_version(k, old_ver, old_app, new_ver, new_app, is_fixed)
+                version_line = print_version(k, old_ver, old_app, new_ver, new_app)
+
+                version_contents.append(version_line)
 
         if doc != None:
-            with open(filepath, "w") as file:
+            with open(file_path, "w") as file:
                 json.dump(doc, file, sort_keys=True, indent=2)
+
+        update_version_in_readme("\n".join(version_contents))
 
 
 if __name__ == "__main__":
